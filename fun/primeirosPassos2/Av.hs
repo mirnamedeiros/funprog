8441d44b28b1cb35f32999fb3f454dd14612362a
Mirna Medeiros <mirnamedeiros@pop-os.localdomain>
2022-09-04 15:45:31 -0300

    melhoria de Av

--- fun/primeirosPassos2/Av.hs
+++ fun/primeirosPassos2/Av.hs
@@ -1,4 +1,6 @@
 module Av where
 
-av :: Integer -> Integer -> Integer
-av a b = div (a + b) 2
+-- av :: Integer -> Integer -> Integer
+-- av a b = div (a + b) 2
+av :: Fractional a => a -> a -> a -- Fractional is a typeclass não um type
+av a b = (a + b) / 2
