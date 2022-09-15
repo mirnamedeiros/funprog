8e9f277ce29b45b21a19b9ea43c80efa23a7d2ce
Mirna Medeiros <mirnamedeiros@pop-os.localdomain>
2022-09-15 18:30:15 -0300

    tipo de error

--- ex/2022-09-06/TipoError.hs
+++ ex/2022-09-06/TipoError.hs
@@ -1,4 +1,4 @@
 -- error é do tipo [Char], seria uma mensagem contendo as informações sobre o erro
 
--- depois de "roubar": error é do tipo [Char] -> a, ou seja, é de um conjunto de caracteres, 
--- que seria a mensagem de erro, para um tipo alfa, que seria o elemento que deu erro.
+-- depois de "roubar": error é do tipo [Char] -> a, ou seja, recebe um conjunto de caracteres, 
+-- que seria a mensagem de erro, e retorna um tipo alfa, que seria o elemento que deu erro.
