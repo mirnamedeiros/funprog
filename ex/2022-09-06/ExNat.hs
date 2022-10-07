a3c93f6a29c5cee154f5c3db7b7609e6f4d570e2
Mirna Medeiros <mirnamedeiros@pop-os.localdomain>
2022-10-07 15:13:20 -0300

    ExNat

--- ex/2022-09-06/ExNat.hs
+++ ex/2022-09-06/ExNat.hs
@@ -29,59 +29,79 @@ instance Show Nat where
 
     -- zero  should be shown as O
     -- three should be shown as SSSO
-    show = undefined
+    show Zero = "O"
+    show (Succ x) = "S" ++ show x
 
+ 
 instance Eq Nat where
-
-    (==) = undefined
+    Zero == Zero = True
+    Succ x == Succ y = x == y 
+    x == y = False
 
 instance Ord Nat where
 
-    (<=) = undefined
+    x <= y = min x y == x 
+    x >= y = max x y == x 
 
     -- Ord does not REQUIRE defining min and max.
     -- Howevener, you should define them WITHOUT using (<=).
     -- Both are binary functions: max m n = ..., etc.
 
-    min = undefined
+    min x Zero = Zero
+    min Zero y = Zero
+    min (Succ x) (Succ y) = Succ (min x y)
 
-    max = undefined
+    max x Zero = x
+    max (Succ x) (Succ y) = Succ (max x y)
 
 isZero :: Nat -> Bool
-isZero = undefined
+isZero Zero = True
+isZero _ = False 
 
 -- pred is the predecessor but we define zero's to be zero
 pred :: Nat -> Nat
-pred = undefined
+pred Zero = Zero
+pred (Succ x) = x
 
 even :: Nat -> Bool
-even = undefined
+even Zero = True
+even (Succ Zero) = False
+even (Succ(Succ x)) = even x
 
 odd :: Nat -> Bool
-odd = undefined
+odd Zero = False
+odd (Succ Zero) = True
+odd (Succ(Succ x)) = odd x
 
 -- addition
 (<+>) :: Nat -> Nat -> Nat
-(<+>) = undefined
+(<+>) x Zero = x 
+(<+>) x (Succ y) = Succ((<+>) x y)
 
 -- This is called the dotminus or monus operator
 -- (also: proper subtraction, arithmetic subtraction, ...).
 -- It behaves like subtraction, except that it returns 0
 -- when "normal" subtraction would return a negative number.
 (<->) :: Nat -> Nat -> Nat
-(<->) = undefined
+ 
+(<->) x Zero = x
+(<->) (Succ x) (Succ y) = (<->) x y
+(<->) x y = Zero
 
 -- multiplication
 (<*>) :: Nat -> Nat -> Nat
-(<*>) = undefined
+(<*>) x Zero = Zero
+(<*>) x (Succ y) = (<+>) ((<*>) x y) x
 
 -- exponentiation
 (<^>) :: Nat -> Nat -> Nat
-(<^>) = undefined
+(<^>) x Zero = Succ Zero
+(<^>) x (Succ y) = (<*>) x ((<^>) x y)
 
 -- quotient
-(</>) :: Nat -> Nat -> Nat
-(</>) = undefined
+(</>) :: Nat -> Nat -> Nat --imcompleto
+(</>) x Zero = error "divisão por zero é indefinido"
+(</>) x y = if x <= y then Zero else (<+>) (Succ Zero) ((</>) ((<->) x y) y)
 
 -- remainder
 (<%>) :: Nat -> Nat -> Nat
@@ -119,22 +139,24 @@ lo = undefined
 --
 
 toNat :: Integral a => a -> Nat
-toNat = undefined
+toNat 0 = Zero
+toNat x = Succ (toNat (x - 1))
 
 fromNat :: Integral a => Nat -> a
-fromNat = undefined
+fromNat Zero = 0
+fromNat (Succ x) = 1 + fromNat x
 
 
 -- Obs: we can now easily make Nat an instance of Num.
-instance Num Nat where
+-- instance Num Nat where
 
-    (+) = (<+>)
-    (*) = (<*>)
-    (-) = (<->)
-    abs n = n
-    signum = sg
-    fromInteger x
-        | x < 0     = undefined
-        | x == 0    = undefined
-        | otherwise = undefined
+    -- (+) = (<+>)
+    -- (*) = (<*>)
+    -- (-) = (<->)
+    -- abs n = n
+    -- signum = sg
+    -- fromInteger x
+    --     | x < 0     = undefined
+    --     | x == 0    = undefined
+    --     | otherwise = undefined
 
