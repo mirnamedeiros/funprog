c64ecf62240fe93bbb19db90e35081fbf6b79295
Mirna Medeiros <mirnamedeiros@pop-os.localdomain>
2022-11-09 22:34:18 -0300

    implementação ExMaybe

--- ex/2022-09-28/ExMaybe.hs
+++ ex/2022-09-28/ExMaybe.hs
@@ -8,32 +8,48 @@ data Maybe a = Nothing | Just a
     deriving (Show, Eq, Ord)
 
 catMaybes :: [Maybe a] -> [a]
-catMaybes = undefined
+catMaybes [] = [] 
+catMaybes x = [ a | Just a <- x]
 
 fromJust :: Maybe a -> a
-fromJust = undefined
+fromJust (Just a) = a
 
 fromMaybe :: a -> Maybe a -> a
-fromMaybe = undefined
+fromMaybe a Nothing = a
+fromMaybe a (Just x) = x
 
 isJust :: Maybe a -> Bool
-isJust = undefined
+isJust (Just _) = True
+isJust _ = False 
 
 isNothing :: Maybe a -> Bool
-isNothing = undefined
+isNothing Nothing = True
+isNothing  _ = False 
 
 listToMaybe :: [a] -> Maybe a
-listToMaybe = undefined
+listToMaybe [] = Nothing
+listToMaybe (x:xs) = (Just x)
 
 mapMaybe :: (a -> Maybe b) -> [a] -> [b]
-mapMaybe = undefined
+mapMaybe f = map fromJust . filter isJust . map f
+-- mapMaybe _ []     = []
+-- mapMaybe f (x:xs) =
+--     let rs = mapMaybe f xs in
+--     case f x of
+--         Nothing -> rs
+--         Just r  -> r:rs
 
 maybe :: b -> (a -> b) -> Maybe a -> b
-maybe = undefined
+maybe n _ Nothing  = n
+maybe _ f (Just x) = f x
 
 maybeToList :: Maybe a -> [a]
-maybeToList = undefined
+maybeToList Nothing = []
+maybeToList (Just x) = [x]
+
+maybeApply :: Maybe (a -> a) -> a -> a
+maybeApply Nothing x = x
+maybeApply (Just f) x = f x
 
 tryToModifyWith :: [Maybe (a -> a)] -> [a] -> [a]
-tryToModifyWith = undefined
-
+tryToModifyWith fs xs = zipWith maybeApply fs xs
