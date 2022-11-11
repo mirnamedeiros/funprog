bb7f4245908fc9b727834bf05f67438376066320
Mirna Medeiros <mirnamedeiros@pop-os.localdomain>
2022-11-10 21:02:09 -0300

    Inhabitants

--- ex/2022-10-27/Inhabitants.hs
+++ ex/2022-10-27/Inhabitants.hs
@@ -8,16 +8,18 @@ module Inhabitants where
 -- f, f', f'', f''', f'''', etc.
 
 cI :: a -> a
-cI = undefined
+cI = \x -> x
 
 cK :: a -> b -> a
-cK = undefined
+cK = \x -> \y -> x
 
+--    ----- f -----    --- g --     x    
 cS :: (a -> b -> c) -> (a -> b) -> (a -> c)
-cS = undefined
+cS f g = \x -> f x (g x)
 
+--     -- f --      -- g --     x
 cB :: (b -> c) -> ((a -> b) -> (a -> c))
-cB = undefined
+cB f g = \x -> f (g x) -- (.)   
 
 cW :: (a -> (a -> b)) -> (a -> b)
 cW = undefined
@@ -26,40 +28,52 @@ pL :: ((a -> b) -> a) -> a
 pL = undefined
 
 p1 :: (a,b) -> a
-p1 = undefined
+p1 = \(x,y) -> x
 
 p2 :: (a,b) -> b
-p2 = undefined
+p2 = \(x,y) -> y
 
 p3 :: (a,b) -> (b,a)
-p3 = undefined
+p3 = \(x,y) -> (y,x) 
 
 p4 :: Either a b -> b
-p4 = undefined
+p4 = \(Right x) -> x 
+
+p4' :: Either a b -> b
+p4' = \(Left x) -> error "accepts only right values"
 
 p5 :: Either a b -> [a]
-p5 = undefined
+p5 = \(Left x) -> [x]
+
+p5' :: Either a b -> [a]
+p5' = \(Right x) -> []
 
 p6 :: Either a b -> (a,b)
 p6 = undefined
 
 p7 :: (a,b) -> Either a b
-p7 = undefined
+p7 = \(x,y) -> Left x
 
 p8 :: (a,b) -> Either a b
-p8 = undefined
+p8 = \(x,y) -> Right y
 
 p9 :: a -> b -> (a,a,b)
-p9 = undefined
+p9 = \x -> \y -> (x,x,y)
 
 q1 :: a -> a -> a
-q1 = undefined
+q1 = \x -> \y -> x
+
+q1' :: a -> a -> a
+q1' = \x -> \y -> y
 
 q2 :: (a,a) -> (a,a)
-q2 = undefined
+q2 (x,y) = (x,y)
 
 q3 :: Either (a,b) (a,c) -> (a, Either b c)
-q3 = undefined
+q3 = \(Right (x,y)) -> (x, Right y)
+
+q3' :: Either (a,b) (a,c) -> (a, Either b c)
+q3' = \(Left (x,z)) -> (x, Left z)
 
 l1 :: [a]
 l1 = undefined
@@ -68,14 +82,14 @@ l2 :: [[a]]
 l2 = undefined
 
 l3 :: [a] -> [a]
-l3 = undefined
+l3 = \[x] -> [x]
 
 l4 :: a -> [a]
-l4 = undefined
+l4 = \x -> [x]
 
 l5 :: [a] -> a
-l5 = undefined
+l5 = \[x] -> x
 
 l6 :: a -> [[a]]
-l6 = undefined
+l6 = \x -> [[x]]
 
